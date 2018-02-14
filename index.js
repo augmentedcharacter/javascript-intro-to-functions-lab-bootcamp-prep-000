9bffb149025d28e6d6575527e05f5ea8be4969ab
Michael Orozco <augmentedcharacter@gmail.com>
2018-02-14 12:32:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-function shout(string){ 
-  return string 
-  string.toUppercase()
+function shout(string) {
+  return string.toUpperCase()
 }
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string)
+  return spy.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log(string)
+  return string.toLowerCase()
+}
+
